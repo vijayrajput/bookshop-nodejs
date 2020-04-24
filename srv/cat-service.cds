082c0d763772b3229f555422e747366d070099f5
Vijay Singh Rajput <vijay.singh.rajput@sap.com>
2020-04-24 15:51:40 +0000

    External Service Call

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,9 +1,18 @@
 using { sap.capire.bookshop as my } from '../db/schema';
+using {API_BUSINESS_PARTNER as external} from './external/API_BUSINESS_PARTNER';
+
 service CatalogService @(path:'/browse') {
 
   @readonly entity Books as SELECT from my.Books {*,
     author.name as author
   } excluding { createdBy, modifiedBy };
+
+  @readonly entity BusinessPartners as projection on external.A_BusinessPartner {
+      key BusinessPartner as ID,
+      FirstName,
+      MiddleName,
+      LastName
+  };
 
   @requires_: 'authenticated-user'
   @insertonly entity Orders as projection on my.Orders;
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,19 +1,29 @@
 /**
  * Implementation for CatalogService defined in ./cat-service.cds
  */
-module.exports = (srv)=>{
 
-  // Use reflection to get the csn definition of Books
-  const {Books} = cds.entities
+const cds = require('@sap/cds')
+
+module.exports = async function () {
+
+  // Use reflection to get the csn definition of CatalogService Entities
+  const {Books,BusinessPartners,Orders} = this.entities
+
+  // get DB Service
+  const dbSrv = await cds.connect.to('db')
+
+  //Get External Service
+  const bupaSrv = await cds.connect.to('API_BUSINESS_PARTNER')
 
   // Add some discount for overstocked books
-  srv.after ('READ','Books', (each)=>{
+  this.after ('READ', Books, each=>{
     if (each.stock > 111) each.title += ' -- 11% discount!'
   })
-
+   // Read Business Partner from External Service
+  this.on('READ', BusinessPartners, async (req) => bupaSrv.tx(req).run(req.query))
   // Reduce stock of books upon incoming orders
-  srv.before ('CREATE','Orders', async (req)=>{
-    const tx = cds.transaction(req), order = req.data;
+  this.before ('CREATE',Orders, async (req)=>{
+    const tx = dbSrv.transaction(req), order = req.data;
     if (order.Items) {
       const affectedRows = await tx.run(order.Items.map(item =>
         UPDATE(Books) .where({ID:item.book_ID})
@@ -24,5 +34,7 @@ module.exports = (srv)=>{
       if (affectedRows.some(row => !row)) req.error(409, 'Sold out, sorry')
     }
   })
+
+  
 
 }
