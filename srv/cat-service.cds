60af172d07f7afb83d18a13df57992fb1abf9bf7
Vijay Singh Rajput <vijay.singh.rajput@sap.com>
2020-04-24 22:04:20 +0000

    delete ext service

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,5 +1,5 @@
 using { sap.capire.bookshop as my } from '../db/schema';
-using {API_BUSINESS_PARTNER as external} from './external/API_BUSINESS_PARTNER';
+
 
 service CatalogService @(path:'/browse') {
 
@@ -7,12 +7,7 @@ service CatalogService @(path:'/browse') {
     author.name as author
   } excluding { createdBy, modifiedBy };
 
-  @readonly entity BusinessPartners as projection on external.A_BusinessPartner {
-      key BusinessPartner as ID,
-      FirstName,
-      MiddleName,
-      LastName
-  };
+ 
 
   @requires_: 'authenticated-user'
   @insertonly entity Orders as projection on my.Orders;
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -7,20 +7,20 @@ const cds = require('@sap/cds')
 module.exports = async function () {
 
   // Use reflection to get the csn definition of CatalogService Entities
-  const {Books,BusinessPartners,Orders} = this.entities
+  const {Books,Orders} = this.entities
 
   // get DB Service
   const dbSrv = await cds.connect.to('db')
 
   //Get External Service
-  const bupaSrv = await cds.connect.to('API_BUSINESS_PARTNER')
+  //const bupaSrv = await cds.connect.to('API_BUSINESS_PARTNER')
 
   // Add some discount for overstocked books
   this.after ('READ', Books, each=>{
     if (each.stock > 111) each.title += ' -- 11% discount!'
   })
    // Read Business Partner from External Service
-  this.on('READ', BusinessPartners, async (req) => bupaSrv.tx(req).run(req.query))
+  //this.on('READ', BusinessPartners, async (req) => bupaSrv.tx(req).run(req.query))
   // Reduce stock of books upon incoming orders
   this.before ('CREATE',Orders, async (req)=>{
     const tx = dbSrv.transaction(req), order = req.data;
